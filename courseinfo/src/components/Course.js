6827b49ba159ad41cf2e804306d6073700d0c639
liwnnfool <powersismaely62922@gmail.com>
2020-12-05 09:31:24 +0800

    update courseinfo. add 1)auto displaying note, not manual input 2) add module

---
+++ courseinfo/src/components/Course.js
@@ -0,0 +1,41 @@
+const Header = ({ header }) => {
+  return <h2>{header}</h2>;
+};
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+const Total = ({ parts }) => {
+  return (
+    <strong>
+      total of {parts.reduce((acc, cur) => (acc += cur.exercises), 0)} exercises
+    </strong>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part part={part} key={part.id} />
+      ))}
+    </div>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header header={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default Course;
